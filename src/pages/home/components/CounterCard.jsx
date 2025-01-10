1a60c71b2919a53606b4832959806a44e770daba
Abdulrahman Moussa <abdelrahmanmoussa111@gmail.com>
2025-01-10 17:06:48 +0200

    (fix) hot fix !

--- src/pages/home/components/CounterCard.jsx
+++ src/pages/home/components/CounterCard.jsx
@@ -1,8 +1,9 @@
 import PropTypes from 'prop-types';
-import { motion, useAnimation, useInView } from 'motion/react';
 import { useEffect, useState, useRef } from 'react';
+import { motion, useAnimation, useInView } from 'motion/react';
+import test from '../../../assets/test.svg';
 
-const CounterCard = ({ num, symbol, title, description }) => {
+const CounterCard = ({ num, symbol, title }) => {
   const [hasAnimated, setHasAnimated] = useState(false);
   const ref = useRef(null);
   const inView = useInView(ref, { threshold: 0.5 });
@@ -24,29 +25,31 @@ const CounterCard = ({ num, symbol, title, description }) => {
   return (
     <div
       ref={ref}
-      className="rounded-site border-[1px] border-secondary-text p-4"
+      className="rounded-site border-[1px] border-secondary-text p-4 flex"
       aria-label={`${title} statistics`}
     >
-      <div className="flex items-center">
-        <span className="text-lg md:text-2xl font-semibold">
-          {symbol}
-        </span>
-        <motion.span
-          className="text-2xl md:text-4xl font-bold text-primary mr-2 ml-1"
-          initial={{ count: 0 }}
-          animate={controls}
-          transition={{ duration: 1.5, ease: 'easeOut' }}
-          onUpdate={(latest) => {
-            document.querySelector(`[data-counter="${title}"]`).innerText =
-              Math.floor(latest.count);
-          }}
-          data-counter={title}
-        >
-          {Math.floor(num)}
-        </motion.span>
-        <span className="text-lg md:text-2xl font-semibold 0">{title}</span>
+      <figure className="w-1/3 flex items-center justify-center">
+        <img src={test} alt={title} className="w-16" />
+      </figure>
+      <div className="flex flex-col items-center justify-center w-2/3">
+        <p className="flex items-center gap-1">
+          <span className="text-lg md:text-2xl font-semibold">{symbol}</span>
+          <motion.span
+            className="text-2xl md:text-4xl font-bold text-primary mr-2 ml-1"
+            initial={{ count: 0 }}
+            animate={controls}
+            transition={{ duration: 1.5, ease: 'easeOut' }}
+            onUpdate={(latest) => {
+              document.querySelector(`[data-counter="${title}"]`).innerText =
+                Math.floor(latest.count);
+            }}
+            data-counter={title}
+          >
+            {Math.floor(num)}
+          </motion.span>
+        </p>
+        <p className="text-lg md:text-2xl font-semibold w-full text-center">{title}</p>
       </div>
-      <p className="text-primary-text mt-2">{description}</p>
     </div>
   );
 };
@@ -55,7 +58,6 @@ CounterCard.propTypes = {
   num: PropTypes.number.isRequired,
   symbol: PropTypes.string,
   title: PropTypes.string.isRequired,
-  description: PropTypes.string.isRequired,
 };
 
 export default CounterCard;
